c3e847770ce0f1eaee3e92bc05278099927fc063
YengSokroza <yengsokroza@gmail.com>
2024-02-23 11:22:23 +0700

    detail pages

---
+++ data/room.js
@@ -0,0 +1,34 @@
+export const rooms = [
+    {
+        id: 1,
+        location: "Sihanouk Ville",
+        price: 109,
+        image: "/images/Detail/2.webp",
+        date: "09 febraury 2024",
+        tag: ["apartment","1 bedroom","27m²"]
+    },
+    {
+        id: 2,
+        location: "Phnom Penh",
+        price: 109,
+        image: "/images/Detail/1.webp",
+        date: "11 May 2024",
+        tag: ["apartment","2 bedroom","27m²"]
+    },
+    {
+        id: 3,
+        location: "Siem Reab",
+        price: 200,
+        image: "/images/Detail/3.webp",
+        date: "21 April 2024",
+        tag: ["apartment","2 bedroom","27m²"]
+    },
+    {
+        id: 4,
+        location: "Kompot",
+        price: 120,
+        image: "/images/Detail/4.webp",
+        date: "03 June 2024",
+        tag: ["apartment","2 bedroom","27m²"]
+    },
+]
---
+++ js/cardComponent.js
@@ -0,0 +1,24 @@
+export const card = (room) => {
+    return `
+    <div class="overflow-hidden rounded-xl shadow-xl">
+    <div>
+        <img class="w-full h-[230px] rounded-t-lg object-cover" src="${room.image || "https://www.svgrepo.com/show/508699/landscape-placeholder.svg"} " alt="image description">
+    </div>
+    <div class="space-y-2 p-4">
+        <p class="text-md  font-bold">${room.price || "Unavailable"}$ <span class="text-xs text-black-50">/ Month <span
+            class="text-xs ">&#40;Bills Included&#41;<span></span>
+        </p>
+        <div>
+            <p class="text-sm font-medium text-black-50">${room.location || "Unavailable"}</p>
+        </div>
+        <div>
+            <a href="" class="px-2 py-1 bg-slate-100 rounded-xl text-xxs text-black-50">${room.tag[0] || "Unavailable"}</a>
+            <a href="" class="px-2 py-1 bg-slate-100 rounded-xl text-xxs text-black-50">${room.tag[1] || "Unavailable"}</a>
+            <a href="" class="px-2 py-1 bg-slate-100 rounded-xl text-xxs text-black-50">${room.tag[2] || "Unavailable"}</a>
+        </div>
+        <p class="text-xs text-black-100">From ${room.date || "Unavailable"}</p>
+    </div>
+    
+</div>
+    `
+}
