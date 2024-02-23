2a5a8af84f8323c73d3e0177cb9085e776731913
YengSokroza <yengsokroza@gmail.com>
2024-02-23 13:06:02 +0700

    list pages

--- js/utilScript.js
+++ js/utilScript.js
@@ -5,8 +5,17 @@ import { rooms } from "../data/room.js"
 import { card } from "./cardComponent.js"
 
 let renderArea = document.querySelector('#render-area')
-
+let houseArea = document.querySelector('#house-area')
+let studioArea = document.querySelector('#studio-area')
+let shRoomArea = document.querySelector('#shRoom-area')
 
 rooms.map((room) => {
     renderArea.innerHTML += card(room)
+    
+})
+
+rooms.map((room) => {
+    houseArea.innerHTML += card(room)
+    studioArea.innerHTML += card(room)
+    shRoomArea.innerHTML += card(room)
 })
